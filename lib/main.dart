c25bd0669c8347b80a1b273f0048428c467670d4
ahmed khaled <ahmed.khaled.z@hotmail.com>
2023-04-02 00:48:24 +0200

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,15 @@
-import 'package:creative_minds_flutter_challenge/utils/constants.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:creative_minds_flutter_challenge/bloc/authentication/authentication_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_flavor/flutter_flavor.dart';
 import 'package:provider/provider.dart';
 import 'app/AppNotifier.dart';
 import 'app/app.dart';
 
+import 'utils/constants.dart';
+
 
 void main() async {
-  // flutter build apk --flavor production -t lib/main_production.dart
-  FlavorConfig(
-    name: "PRODUCTION",
-    variables: {
-      "baseUrl": "https://api.github.com",
-    },
-  );
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   final AuthenticationBloc authenticationBloc = AuthenticationBloc();
--- lib/utils/end_boints.dart
+++ lib/utils/end_boints.dart
@@ -1,6 +1,5 @@
-import 'package:flutter_flavor/flutter_flavor.dart';
-String baseUrl = FlavorConfig.instance.variables["baseUrl"];
-String repos = "$baseUrl/users/square/repos";
+const baseUrl = "https://api.github.com";
+const repos = "$baseUrl/users/square/repos";
 
 
 
